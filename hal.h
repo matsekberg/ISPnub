c27b9936ec8e2a2479243218f6e7f81da57c8130
Mats Ekberg <matsekberg@gmail.com>
2018-01-30 10:06:53 +0100

    Update hal.h
    Started on USART implementation

--- hal.h
+++ hal.h
@@ -5,7 +5,8 @@
  *
  * @author Thomas Fischl
  * @copyright (c) 2013 Thomas Fischl
- * 
+
+* 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -24,35 +25,43 @@
 #ifndef _HAL_
 #define _HAL_
 
+#define USART_BAUDRATE 38400
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
 // ******************************** ATmega8 *************************************
 #if defined (__AVR_ATmega8__)
 
 #define DEFINE_DATAPOINTER uint16_t scriptdata_p = (uint16_t)scriptdata
-
 #define hal_init() DDRC = 0x03; PORTC = 0xfe; // all inputs except PC0 and PC1
 #define hal_getSwitch() ((PINC & (1 << PC2)) == 0)
 #define hal_setLEDgreen(x) PORTC = (PORTC & ~(1 << PC0)) | ((!x) << PC0)
 #define hal_setLEDred(x) PORTC = (PORTC & ~(1 << PC1)) | ((!x) << PC1)
+#define hal_initUSART() UBRR0L = BAUD_PRESCALE; UBRR0H = (BAUD_PRESCALE >> 8); UCSR0B = (1 << TXEN0);
+#define hal_txUSART(x) while (!( UCSRA0 & (1 << UDRE0))); UDR0 = data;
 
 #define flash_readbyte(x) pgm_read_byte(x)
 
 #define	ISP_OUT   PORTB
 #define ISP_IN    PINB
+
 #define ISP_DDR   DDRB
 #define ISP_RST   PB2
 #define ISP_MOSI  PB3
 #define ISP_MISO  PB4
 #define ISP_SCK   PB5
 
-// ******************************** ATmega128P ***********************************
-#elif defined (__AVR_ATmega1284P__)
+// ******************************** ATmega324P ***********************************
+#elif defined (__AVR_ATmega328P__)
 
-#define DEFINE_DATAPOINTER uint32_t scriptdata_p = FAR(scriptdata);
+#define hal_initUSART() UBRR0L = BAUD_PRESCALE; UBRR0H = (BAUD_PRESCALE >> 8); UCSR0B = (1 << TXEN0);
+#define hal_txUSART(x) while (!( UCSRA0 & (1 << UDRE0))); UDR0 = data;#define DEFINE_DATAPOINTER uint32_t scriptdata_p = FAR(scriptdata);
 
 #define hal_init() DDRC = (1 << PC3); DDRD = (1 << PD4); PORTD = ~(1 << PD4); // all inputs except PC3 and PD4
 #define hal_getSwitch() ((PIND & (1 << PD3)) == 0)
 #define hal_setLEDred(x) PORTC = (PORTC & ~(1 << PC3)) | ((!x) << PC3)
 #define hal_setLEDgreen(x) PORTD = (PORTC & ~(1 << PD4)) | ((!x) << PD4)
+#define hal_initUSART() UBRR0L = BAUD_PRESCALE; UBRR0H = (BAUD_PRESCALE >> 8); UCSR0B = (1 << TXEN0);
+#define hal_txUSART(x) while (!( UCSRA0 & (1 << UDRE0))); UDR0 = data;
 
 #define flash_readbyte(x) pgm_read_byte_far(x)
 
@@ -67,6 +76,29 @@
 #define TCCR0 TCCR0B
 #define TIMSK TIMSK0
 
+// ******************************** ATmega1284P ***********************************
+#elif defined (__AVR_ATmega1284P__)
+
+#define DEFINE_DATAPOINTER uint32_t scriptdata_p = FAR(scriptdata);
+
+#define hal_init() DDRC = (1 << PC3); DDRD = (1 << PD4); PORTD = ~(1 << PD4); // all inputs except PC3 and PD4
+#define hal_getSwitch() ((PIND & (1 << PD3)) == 0)
+#define hal_setLEDred(x) PORTC = (PORTC & ~(1 << PC3)) | ((!x) << PC3)
+#define hal_setLEDgreen(x) PORTD = (PORTC & ~(1 << PD4)) | ((!x) << PD4)
+
+
+#define flash_readbyte(x) pgm_read_byte_far(x)
+
+#define	ISP_OUT   PORTB
+#define ISP_IN    PINB
+#define ISP_DDR   DDRB
+#define ISP_RST   PB4
+#define ISP_MOSI  PB5
+#define ISP_MISO  PB6
+#define ISP_SCK   PB7
+
+#define TCCR0 TCCR0B
+#define TIMSK TIMSK0
 // ****************************** unknown device *********************************
 #else 
   #error "MCU not supported by HAL"
